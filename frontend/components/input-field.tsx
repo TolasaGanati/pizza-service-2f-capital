d0bff22a1644c0d86ff7fbb852c9a21cf79b2734
TolasaGanati <tolog766@gmail.com>
2024-10-02 17:48:02 +0300

    commit

--- frontend/components/input-field.tsx
+++ frontend/components/input-field.tsx
@@ -1,34 +1,52 @@
-import { Box, TextField, Typography } from '@mui/material'
-import { FieldError } from 'react-hook-form'
-import { SxProps, Theme } from '@mui/system'
+import { Box, SxProps, TextField, Typography } from "@mui/material";
+import { FieldError } from "react-hook-form";
+import { styled } from "@mui/system";
+import { Theme } from "@mui/system";
+
+const StyledBox = styled(Box)({
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px", // Replace 'gap: 1' with a valid unit like '8px'
+});
 
 type InputFieldProps = {
-    label: string
-    type: string
-    name: string
-    register: any
-    error?: FieldError | any
-    InputLabelProps?: any
-    sx?: SxProps<Theme>
-}
+  label: string;
+  type: string;
+  name: string;
+  register: any;
+  error?: FieldError | any;
+  InputLabelProps?: any;
+  sx?: SxProps<Theme>;
+};
 
-const InputField = ({ label, type, name, register, error, InputLabelProps, sx }: InputFieldProps) => {
-    return (
-        <Box sx={{ width: "100%", display: "flex", gap: 1, flexDirection: "column", ...sx }}>
-            <TextField
-                {...register(name, { valueAsNumber: type === "number" })}
-                label={label}
-                type={type}
-                variant="outlined"
-                InputLabelProps={InputLabelProps}
-            />
-            {error?.message && (
-                <Typography sx={{ color: "red" }}>
-                    {error.message.toString()}
-                </Typography>
-            )}
-        </Box>
-    )
-}
+const InputField = ({
+  label,
+  type,
+  name,
+  register,
+  error,
+  InputLabelProps,
+  sx,
+}: InputFieldProps) => {
+  return (
+    <StyledBox sx={sx}>
+      {" "}
+      {/* you can still pass sx if needed */}
+      <TextField
+        {...register(name, { valueAsNumber: type === "number" })}
+        label={label}
+        type={type}
+        variant="outlined"
+        InputLabelProps={InputLabelProps}
+      />
+      {error?.message && (
+        <Typography style={{ color: "red" }}>
+          {error.message.toString()}
+        </Typography>
+      )}
+    </StyledBox>
+  );
+};
 
-export default InputField
+export default InputField;
--- frontend/utils/types.ts
+++ frontend/utils/types.ts
@@ -1,10 +1,9 @@
 import { z } from "zod";
-import { addMenuSchema, createBookSchema, LoginSchema, registerSchema } from "./schema";
+import { addMenuSchema, LoginSchema, registerSchema } from "./schema";
 
 export type RegisterFormTypes = z.infer<typeof registerSchema>
 export type LoginFormTypes = z.infer<typeof LoginSchema>
 export type AddMenuTypes = z.infer<typeof addMenuSchema>;  
-export type CreateBookFormTypes = z.infer<typeof createBookSchema>;
 
 
 
