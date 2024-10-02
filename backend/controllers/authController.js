84bf4a110b3ab95ade08146652c1765c823eaaaf
TolasaGanati <tolog766@gmail.com>
2024-10-02 16:54:10 +0300

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -75,16 +75,15 @@ export const login = async (req, res) => {
 
     const { password: userPassword, ...userInfo } = user;
 
-   res
+    res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None',
+        secure: false, // Disable secure in development as you're not using HTTPS
+        sameSite: 'Lax', // Use Lax in development to avoid cross-site issues
         maxAge: age,
-      })
+      })       
       .status(200)
-      .json({ role: role.name, ...userInfo });
-
+      .json({ role: user.role, ...userInfo });
   } catch (err) {
     if (err instanceof z.ZodError) {
       return res.status(400).json({ error: "Invalid requested data" });
@@ -94,6 +93,18 @@ export const login = async (req, res) => {
     res.status(500).json({ message: err });
   }
 };
+/////////////change specific part the above for production to
+
+  // res
+  //     .cookie("token", token, {
+  //       httpOnly: true,
+  //       secure: process.env.NODE_ENV === 'production',
+  //       sameSite: 'None',
+  //       maxAge: age,
+  //     })
+  //     .status(200)
+  //     .json({ role: role.name, ...userInfo });
+
 
 // Logout function
 export const logout = (req, res) => {
