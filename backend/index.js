22977e09e4133fbb214b4cad11b8d24d1dd09331
TolasaGanati <99481807+TolasaGanati@users.noreply.github.com>
2024-10-05 09:06:39 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({  origin: 'https://pizza-ordering-service-2f-capital-frontend.vercel.app',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // List of allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // List of allowed headers
+    credentials: true }));
 app.use(express.json());
 app.options('*', cors()); 
 // Routes
